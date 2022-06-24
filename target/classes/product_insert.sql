02c356b0ca3e4aad90e5304dd444ffd32a71f88d
returnNull <91457054+nullpointerexception11@users.noreply.github.com>
2022-06-24 21:39:15 +0300

    Delete target/classes directory

--- target/classes/product_insert.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO products( ID, NAME, PRICE, CREATED_DATE) VALUES (	1	,'Mavi Elbise',	200	, now());
-INSERT INTO products( ID, NAME, PRICE, CREATED_DATE) VALUES (	2	,'Kırmızı Elbise',	300	, now());
-INSERT INTO products( ID, NAME, PRICE, CREATED_DATE) VALUES (	3	,'Mavi Gömlek',	500	, now());
-INSERT INTO products( ID, NAME, PRICE, CREATED_DATE) VALUES (	4	,'Sarı Gömlek',	450	, now());
-INSERT INTO products( ID, NAME, PRICE, CREATED_DATE) VALUES (	5	,'HP ',	15000	, now());
-INSERT INTO products( ID, NAME, PRICE, CREATED_DATE) VALUES (	6	,'MSI',	20000	, now());
-INSERT INTO products( ID, NAME, PRICE, CREATED_DATE) VALUES (	7	,'Orta Sehpa',	600	, now());
-
-----This source is sampled from Asim Kilic's Github Repo
--- target/classes/user_insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users(ID, NAME, SURNAME, EMAIL, PHONE) VALUES(1, N'ASIM', N'KILIÇ', N'asimkilic@patika.dev', N'+90123456789');
-INSERT INTO users(ID, NAME, SURNAME, EMAIL, PHONE) VALUES(2, N'JAMES', N'GOSLING', N'james@patika.dev', N'+90119653789');
-INSERT INTO users(ID, NAME, SURNAME, EMAIL, PHONE) VALUES(3, N'DENNIS', N'RITCHIE', N'dennis@patika.dev', N'+90158366789');
-INSERT INTO users(ID, NAME, SURNAME, EMAIL, PHONE) VALUES(4, N'LINUS', N'TORVALDS', N'linus@patika.dev', N'+90123446289');
-
-----This source is sampled from Asim Kilic's Github Repo
